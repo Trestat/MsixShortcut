2c1e3ac8f6eb0c55f09eabeed7cd21db9e08234e
Blake Burgess <trinitek@live.com>
2023-12-17 06:29:42 -0500

    Add test project

---
+++ src/MsixShortcut.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MsixShortcut.Tests
+{
+    public sealed class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
